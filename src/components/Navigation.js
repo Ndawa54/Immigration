1c2821c38a76372bc7892f2a096b80ea3de991f0
Ndawa54 <emmanuelndawa53@gmail.com>
2025-02-08 19:58:09 +0200

    Getting there

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,27 +1,51 @@
-import { Menu } from '@mui/icons-material'
-import { AppBar, Button, IconButton, Stack, Toolbar, Typography } from '@mui/material'
-import React from 'react'
+import { Logout, Menu, Person } from '@mui/icons-material'
+import { AppBar, Button, Drawer, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Stack, Toolbar, Typography } from '@mui/material'
+import React, { useState } from 'react'
 
 
 export default function Navigation() {
-  return (
-    <div>
-        <AppBar>
-            <Toolbar>
-                <IconButton color='inherit'>
-                    <Menu />
-                </IconButton>
-                <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1
-                }}> Home</Typography>
+    const [open, setOpen] = useState(false)
 
-                <Stack direction="row" spacing={2}>
-                    <Button color='inherit'>News</Button>
-                    <Button color='inherit'>Weather</Button>
-                    <Button color='inherit'>Settings</Button>
-                </Stack>
+    const handleClick = () => {
+        setOpen(true)
+    }
+    return (
+        <div>
+            <AppBar>
+                <Toolbar>
+                    <IconButton color='inherit' onClick={handleClick}>
+                        <Menu />
+                    </IconButton>
+                    <Typography variant="h6" noWrap component="div" sx={{
+                        flexGrow: 1
+                    }}> </Typography>
 
-            </Toolbar>
-        </AppBar>
-    </div>
-  )
+                    <Stack direction="row" spacing={2}>
+                        <Button color='inherit'>Home</Button>
+                        <Button color='inherit'>Track</Button>
+                        <Button color='inherit'>Contacts</Button>
+                    </Stack>
+
+                </Toolbar>
+                <Drawer anchor="left" open={open} onClose={() => { setOpen(false) }} >
+
+                    <List sx={{ spacing: '2', width: '30ch', display: 'block', role: 'presentation' }}>
+                        <ListItem>
+                            <ListItemButton>
+                                <ListItemIcon color='inherit'><Person /></ListItemIcon>
+                                <ListItemText>Profile</ListItemText>
+                            </ListItemButton>
+                        </ListItem>
+                        <ListItem>
+                            <ListItemButton>
+                                <ListItemIcon color='inherit'><Logout /></ListItemIcon>
+                                <ListItemText>Logout</ListItemText>
+                            </ListItemButton>
+                        </ListItem>
+
+                    </List>
+                </Drawer>
+            </AppBar>
+        </div>
+    )
 }
